88d24864152c697304d1c16763c1935204ccf821
Edikan Odokwo <edetedikan647@gmail.com>
2025-06-01 17:27:31 +0100

    changed user.post in usermodalprops

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -66,13 +66,13 @@ const handleAddUser = (newUser: UserData) => {
 };
 
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users: UserProps[] = await response.json();
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
 
   return {
     props: {
-      users,
-    },
-  };
+      posts
+    }
+  }
 }
 export default Users;
